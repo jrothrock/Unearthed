8cac2018cc7257ef872e29270513d40225371ee2
Julio Gutierrez <bubuntux@gmail.com>
2017-09-23 11:27:15 -0600

    almost there!

--- arw_to_tiff.py
+++ arw_to_tiff.py
@@ -3,7 +3,6 @@ import os
 from PIL import Image
 
 from data_prep_rename import chopImage
-
 from excelReader_rename import readExcelFiles
 
 inputDir = './img/sets/'
@@ -15,6 +14,6 @@ for file in os.listdir(inputDir):
     if ".tiff" not in file:
         continue
     print('processing ' + file)
-    width, height = Image.open(inputDir + file).size
-    chopImage(excelSheets, file, width, height, outputDir)
+    img = Image.open(inputDir + file)
+    chopImage(excelSheets, img, outputDir)
     break
--- data_prep_rename.py
+++ data_prep_rename.py
@@ -1,17 +1,6 @@
-# This script prepares the data set by:
-#  1. partitioning cropped multi-rock image files into single rock image files. 
-#  2. reducing the size of each rock image 
-#  3. saving the resulting images and labels to data.pickle
-#
-# It is assumed that all the cropped multi-rock image files are of the same size, 
-# resulting in individual images of size 722 x 469 pixels.  These rocks are then resized to 224 x 136.
-#
-# To run the script, execute "python data_prep.py" 
+def chopImage(excelSheets, img, output):
+    width, height = img.size
 
-from scipy.misc import imread, imsave, imresize
-
-
-def chopImage(excelSheets, file, width, height, output):
     rows = [1, 2, 3, 4, 5]
     cols = ['A', 'B', 'C', 'D', 'E']
     R = len(rows)
@@ -19,34 +8,22 @@ def chopImage(excelSheets, file, width, height, output):
 
     print("in chopImage")
 
-    HH = width  # height of rock image
-    WW = height  # width of rock image
+    rock_width = width / 5
+    rock_height = height / 5
 
-    # max_size = int(literal_eval(sys.argv[1]))
-    max_size = 224
-
-    aspect_ratio = float(WW) / HH if HH > WW else float(HH) / WW
-
-    aspect_height = max_size if HH > WW else int(round(max_size * aspect_ratio, 0))
-
-    aspect_width = max_size if WW > HH else int(round(max_size * aspect_ratio, 0))
-
-    image_size = (aspect_height, aspect_width,)
-
-    print("aspect_rate = " + str(aspect_ratio))
-    print("aspect_height = " + str(aspect_height))
-    print("aspect_width = " + str(aspect_width))
-
-    rocks = imread('./img/sets/' + file)
+    filename = img.filename.split("/")[-1]
 
     for j in range(R):
         for k in range(C):
-            rock = imresize(rocks[j * HH:j * HH + HH, k * WW:k * WW + WW, :], image_size)
-            file_name = file.replace(".tiff", "") + '_' + str(rows[j]) + cols[k] + '.jpg'
+            rock = img.crop((rock_width * k, rock_height * j, rock_width, rock_height))
+            file_name = filename.replace(".tiff", "") + '_' + cols[k] + str(rows[j]) + '.tiff'
             grid = "Grid " + file_name.split("_")[0]
             print("Grid=" + str(grid))
             print("cols=" + str(cols[k]))
             print("row=" + str(rows[j]))
             print('type=' + excelSheets[grid][cols[k]][rows[j]])
             final_file = output + '/' + excelSheets[grid][cols[k]][rows[j]] + '/' + file_name
-            imsave(final_file, rock)
+            print('saving ' + final_file)
+            rock.save(final_file)
+        break
+    img.close()
